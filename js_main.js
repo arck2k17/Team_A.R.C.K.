d4665256b9f09dc38218ff4f00018ed3d79c28f0
arck2k17 <30935141+arck2k17@users.noreply.github.com>
2017-08-17 13:04:28 -0400

    Update js_main.js

--- js_main.js
+++ js_main.js
@@ -1,1 +1,9 @@
-function 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "275px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
